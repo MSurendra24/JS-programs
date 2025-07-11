374406b4a3f37017ca149ede7b49cb1c871a4846
MSurendra24 <mettelasurendra2004@gmail.com>
2025-07-11 12:49:29 +0530

    programs

---
+++ 3large.js
@@ -0,0 +1,15 @@
+let a=9999;
+let b=999;
+let c=99;
+if(a>b && a>c){
+    console.log("a is big")
+}
+else if(b>a && b>c){
+    console.log("b is big")
+}
+else if(c>a && c>b){
+    console.log("c is big")
+}
+else{
+    console.log("no num is big")
+}
---
+++ least.js
@@ -0,0 +1,11 @@
+let num=9
+let val=8
+if(num<val){
+    console.log("num is small ")
+}
+else if(val<num){
+    console.log("val is small ")
+}
+else{
+    console.log("equal")
+}
