8edcc92cdf27173f9690d6b42fb82c23d0488284
MSurendra24 <mettelasurendra2004@gmail.com>
2025-07-12 06:55:41 +0530

    start

---
+++ 3least.js
@@ -0,0 +1,15 @@
+let a=10;
+let b=20;
+let c=30;
+if(a<b && a<c){
+    console.log("a is least num")
+}
+else if(b<a && b<c){
+    console.log("b is least num")
+}
+else if(c<a && c<b){
+    console.log("c is least num")
+}
+else{
+    console.log("all nums are equal")
+}
